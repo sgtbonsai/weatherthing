7d81313b9f9d70f05c74666a0c2558c769e35f86
Leo Tran <62197870+sgtbonsai@users.noreply.github.com>
2022-07-10 20:30:33 -0500

    Create script.js
    nightmare 👹

---
+++ script.js
@@ -0,0 +1,60 @@
+//TEXAS
+const weatherTX =  fetch('https://api.openweathermap.org/data/2.5/weather?id=4684888&appid=a259da0eda6a4eb9f60f0d3d55f4997a&units=imperial')
+  .then(function(resp) { return resp.json() }) 
+  .then(function(data) {
+    console.log(data);
+    TXTEMP.innerHTML = Math.round(data.main.temp) + "˚F"
+  })
+    
+  const TXTemp = document.getElementById("tempTX");
+
+const TXweather =  fetch('https://api.openweathermap.org/data/2.5/weather?id=4684888&appid=a259da0eda6a4eb9f60f0d3d55f4997a&units=imperial')
+  .then(function(resp) { return resp.json() }) 
+  .then(function(data) {
+    console.log(data);
+    TXFEELS.innerHTML = Math.round(data.main.feels_like) + "˚F" })
+  const TXFeels = document.getElementById("feelsTX");
+
+
+//WASHINGTON
+ const weatherWA =  fetch('https://api.openweathermap.org/data/2.5/weather?id=5809844&appid=a259da0eda6a4eb9f60f0d3d55f4997a&units=imperial')
+  .then(function(resp) { return resp.json() }) 
+  .then(function(data) {
+    console.log(data);
+    WATEMP.innerHTML = Math.round(data.main.temp) + "˚F"
+  })
+
+const WATemp = document.getElementById("tempWA");
+
+//NEW YORK
+ const weatherNY =  fetch('https://api.openweathermap.org/data/2.5/weather?id=5128581&appid=a259da0eda6a4eb9f60f0d3d55f4997a&units=imperial')
+  .then(function(resp) { return resp.json() }) 
+  .then(function(data) {
+    console.log(data);
+    NYTEMP.innerHTML = Math.round(data.main.temp) + "˚F"
+  })
+
+const NYTemp = document.getElementById("tempWA");
+
+//FLORIDA
+ const weatherFL =  fetch('https://api.openweathermap.org/data/2.5/weather?id=4164138&appid=a259da0eda6a4eb9f60f0d3d55f4997a&units=imperial')
+  .then(function(resp) { return resp.json() }) 
+  .then(function(data) {
+    console.log(data);
+    FLTEMP.innerHTML = Math.round(data.main.temp) + "˚F"
+  })
+
+const FLTemp = document.getElementById("tempFL");
+
+//CALIFORNIA
+ const weatherCA =  fetch('https://api.openweathermap.org/data/2.5/weather?id=5368361&appid=a259da0eda6a4eb9f60f0d3d55f4997a&units=imperial')
+  .then(function(resp) { return resp.json() })
+  .then(function(data) {
+    console.log(data);
+    CATEMP.innerHTML = Math.round(data.main.temp) + "˚F"
+  })
+
+const CATemp = document.getElementById("tempCA");
+
+document.getElementById('icon').src = `http://openweathermap.org/img/w/${d.weather[0].icon}.png`;
+ 
